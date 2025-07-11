50fc57bc6fd6043ccfcfbeaa485f9aafa2ad1137
tjg9265 <tjg9265@yahoo.com>
2025-07-10 19:17:38 -0600

    update 11

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,5 +1,5 @@
-const yearSpan = document.getElementById("year");
-yearSpan.textContent = new Date().getFullYear();
-const lastModified = document.getElementById("lastModified");
-lastModified.textContent = `Last Modified: ${document.lastModified}`;
+document.querySelector("#year").textContent = new Date().getFullYear();
 
+const lastModified = new Date(document.lastModified);
+document.querySelector("#lastModified").textContent =
+  "Last Modified: " + lastModified.toLocaleString();
