ede1b171a73b4d2b6094a49acc443774534df363
tjg9265 <tjg9265@yahoo.com>
2025-07-26 20:09:52 -0600

    update update update

--- scripts/filtered-temples.js
+++ scripts/filtered-temples.js
@@ -32,3 +32,29 @@ function outputTemples(temples) {
     container.appendChild(card);
   });
 }
+
+const url = "../wdd131/data/temples.json";
+
+fetch(url)
+  .then(res => res.json())
+  .then(data => {
+    displayTemples(data);
+  });
+
+function displayTemples(temples) {
+  const container = document.querySelector(".temple-grid");
+  container.innerHTML = "";
+
+  temples.forEach((temple) => {
+    const card = document.createElement("div");
+    card.classList.add("temple-card");
+    card.innerHTML = `
+      <h3>${temple.name}</h3>
+      <img src="${temple.imageUrl}" alt="${temple.altText}">
+      <p><strong>Location:</strong> ${temple.location}</p>
+      <p><strong>Dedicated:</strong> ${temple.dedicationDate}</p>
+      <p><strong>Area:</strong> ${temple.area.toLocaleString()} sq ft</p>
+    `;
+    container.appendChild(card);
+  });
+}
